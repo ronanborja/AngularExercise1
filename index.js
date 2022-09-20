2b905901d5c01d8a4468177fa8b76c137cc10a31
ronanborja <ronanborja1205@gmail.com>
2022-09-20 15:58:54 +0800

    first commit

---
+++ index.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.testFunction = void 0;
+const testFunction = () => {
+    console.log("Hello to Typescript");
+};
+exports.testFunction = testFunction;
+(0, exports.testFunction)();
---
+++ index.ts
@@ -0,0 +1,5 @@
+export const testFunction = () => {
+    console.log("Hello to Typescript")
+}
+
+testFunction();
